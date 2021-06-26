92e87d374ad0cb6cce958c7f8de354b92f27cbb8
Igor Ulian <igorulian@outlook.com>
2021-06-25 22:40:22 -0300

    starting backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get((req,res) => {
+    return res.jso
+})
