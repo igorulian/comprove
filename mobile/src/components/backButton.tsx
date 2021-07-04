612ce4fada5150acc3e3a9b0fb2c822a4017ad88
Igor Ulian <igorulian@outlook.com>
2021-07-03 21:45:29 -0300

    add back button component

---
+++ mobile/src/components/backButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useNavigation } from '@react-navigation/native';
+
+
+const BackButton:React.FC = () =>{
+
+    const navigation = useNavigation();
+    return(
+        <TouchableOpacity style={styles.container} onPress={() => {navigation.goBack()}}>
+            <MaterialCommunityIcons name="arrow-left" color={'#f78139'} size={30} />
+        </TouchableOpacity>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        margin: 20
+    }
+})
+
+export default BackButton
