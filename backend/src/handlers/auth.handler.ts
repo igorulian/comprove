f4bb821562978a02d4cef323ea31de7cdd52a0fb
Igor Ulian <igorulian@outlook.com>
2021-07-04 21:02:19 -0300

    fixed removed username from user schema

--- backend/src/handlers/auth.handler.ts
+++ backend/src/handlers/auth.handler.ts
@@ -41,7 +41,7 @@ async function handleRegister(req:Request, res:Response){
     const {email,password} = req.body
 
     if(!password || !email)
-    return res.status(400).send({error: 'Preencha todos os campos'}) 
+        return res.status(400).send({error: 'Preencha todos os campos'}) 
     
     if(await User.findOne({email}))
         return res.status(400).send({error: 'Usuário já existente'})
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,11 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 
-mongoose.connect(`${process.env.MONGO_CONNECT_LINK}`,{ useUnifiedTopology: true })
+mongoose.connect(`${process.env.MONGO_CONNECT_LINK}`,{ useUnifiedTopology: true }).then(() => {
+    console.log('DB Conectada com sucesso')
+}).catch(() => {
+    console.log('Erro ao conenctar db')
+})
 
 app.listen(3002)
 
