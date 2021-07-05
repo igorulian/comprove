fb1b21728840ee8f4da59f865c52040deeae24a9
Igor Ulian <igorulian@outlook.com>
2021-07-05 11:31:17 -0300

    new tab navigation style

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -5,19 +5,44 @@ import Home from './pages/Home'
 import PerDate from './pages/PerDate';
 import PerCategory from './pages/PerCategory';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { GestureResponderEvent, TouchableOpacity, View } from 'react-native';
 
 const Tab = createBottomTabNavigator()
 
 const color:string = '#f78139'
 
+type ButtonTypes = {
+    children: React.ReactNode,
+    onPress: any
+}
+
+const UploadButton: React.FC|any = ({children, onPress}:ButtonTypes) => {
+
+    return(
+        <TouchableOpacity style={{
+            top: -30,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 70,
+            height: 70,
+            borderRadius: 35,
+            backgroundColor: '#f78139'
+        }}
+        onPress={onPress}>
+            {children}
+
+        </TouchableOpacity>
+    )
+}
+
 export const TabRoutes = () => (
-    <Tab.Navigator initialRouteName="Home" tabBarOptions={{activeTintColor: '#f78139'}}>
+    <Tab.Navigator initialRouteName="Home" tabBarOptions={{activeTintColor: '#f78139', showLabel: false}}>
         <Tab.Screen
             name="Data"
             component={PerDate}        
             options={{
                 tabBarLabel: 'Data',
-                tabBarIcon: ({color}) => <MaterialCommunityIcons name="calendar-text" color={color} size={30} />
+                tabBarIcon: ({color,focused}) => <MaterialCommunityIcons name="calendar-text" color={color} size={30} />
             }}  
         />  
                 
@@ -25,8 +50,9 @@ export const TabRoutes = () => (
             name="Home"
             component={Home}
             options={{
-                tabBarLabel: 'Adicionar',
-                tabBarIcon: ({color}) => <MaterialCommunityIcons name="plus-circle" color={color} size={30} />
+                tabBarLabel: 'Teste',
+                tabBarIcon: () => (<MaterialCommunityIcons name="arrow-up" color={'#fff'} size={40} />),
+                tabBarButton: (props) => (<UploadButton {...props}/>) 
             }}  
         />
         
