53a7e1534d6eee62d680382cfb77bf6e53a8710d
Igor Ulian <igorulian@outlook.com>
2021-07-09 12:09:22 -0300

    add camera / file upload buttons to 'home'

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
 import { useContext } from 'react';
-import {SafeAreaView, Text, TouchableOpacity} from 'react-native'
+import {Dimensions, SafeAreaView, Text, TouchableOpacity, View} from 'react-native'
 import AuthContext from '../../context/auth';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import styles from './styles'
+
+
+const screenWidth = Dimensions.get('window').width
+const Line:React.FC = () => (
+    <View style={{
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        width: screenWidth - 150
+    }}/>
+)
+
+const ConfigButton:React.FC = () => (
+    <TouchableOpacity style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        margin: 20
+    }}>
+        <MaterialCommunityIcons name='cog' size={30} color='#333'/>
+    </TouchableOpacity>
+)
 
 export default function Home(){
 
     const {signOut} = useContext(AuthContext)
     
     return (
-        <SafeAreaView style={{alignItems: 'center', justifyContent: 'center'}}>
-            <Text> HOMI </Text>
-            <TouchableOpacity onPress={() => {signOut()}}>
-                <Text> LOGOUT </Text>
+        <SafeAreaView style={styles.container}>
+            
+            <ConfigButton/>
+
+            <TouchableOpacity style={styles.cameraButton}>
+                <MaterialCommunityIcons name='camera' size={100} color='#0b465e'/>
+                <Text style={styles.buttonText}> Tirar uma foto </Text>
+                <Text style={styles.buttonText}>  do comprovante </Text>
+            </TouchableOpacity>
+
+            <Line/>
+            
+            <TouchableOpacity style={styles.fileButton}>
+                <MaterialCommunityIcons name='file' size={100} color='#0b465e'/>
+                <Text style={styles.buttonText}> Escolher arquivo </Text>
+                <Text style={styles.buttonText}> da galeria </Text>
             </TouchableOpacity>
 
         </SafeAreaView>
---
+++ mobile/src/pages/Home/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    cameraButton: {
+        marginBottom: 70,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+    ,
+    fileButton: {
+        marginTop: 70,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText:{
+        color: '#0b465e',
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
+})
+
+export default styles
