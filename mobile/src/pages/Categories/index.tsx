6a665702d5410872ef6ba5aaf6b1b2b23f387c5b
Igor Ulian <igorulian@outlook.com>
2021-07-08 21:36:28 -0300

    add pull to refresh to category list

--- mobile/src/pages/Categories/index.tsx
+++ mobile/src/pages/Categories/index.tsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import { useContext } from 'react'
-import {Text,SafeAreaView, FlatList, TouchableOpacity} from 'react-native'
+import {Text,SafeAreaView, FlatList, TouchableOpacity, Alert} from 'react-native'
 import AuthContext, { ICategory } from '../../context/auth'
 import styles from './styles'
 import Category from './category'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import api, { authorizaton } from '../../services/api'
 
 const Categories:React.FC = () =>{
     const {user} = useContext(AuthContext)
     const categories = user?.categories
     const navigation = useNavigation()
+    const [refreshing, setRefresh] = useState(false)
+    const {token,updateUserCategories} = useContext(AuthContext)
+
+    async function refreshCategories(){
+        setRefresh(true)
+        await api.get('/data', authorizaton(token))
+        .then(async (response) => {
+            const updatedCategories = response.data.categories
+            await updateUserCategories(updatedCategories)
+        })
+        .catch((error) => {
+            Alert.alert('Ops!', `${error.response.data.error}`)
+        })
+
+        setRefresh(false)
+    }
 
     function goToAdd(){
         navigation.navigate('add')
@@ -31,7 +48,9 @@ const Categories:React.FC = () =>{
                 data={categories}
                 numColumns={1}
                 keyExtractor={item => item._id}
-                renderItem={({ item }: { item: ICategory }) => <Category category={item}/> }
+                renderItem={({ item }: { item: ICategory }) => <Category category={item}/>}
+                onRefresh={() => {refreshCategories()}}
+                refreshing={refreshing}
             />
             <AddButton/>
         </SafeAreaView>
