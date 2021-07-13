7c9667e08143d64e24b78bd35799caed698d6c9d
Igor Ulian <igorulian@outlook.com>
2021-07-13 13:57:27 -0300

    change aplication name

--- mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
 import com.horcrux.svg.SvgPackage;
 import com.reactnativecommunity.androidprogressbar.RNCProgressBarPackage;
 import com.facebook.react.ReactInstanceManager;
