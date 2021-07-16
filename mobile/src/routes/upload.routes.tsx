169617297a215d60c252490a5e412eec675b5ead
Igor Ulian <igorulian@outlook.com>
2021-07-16 19:09:57 -0300

    fixed upload top bar style

--- mobile/src/routes/upload.routes.tsx
+++ mobile/src/routes/upload.routes.tsx
@@ -9,29 +9,29 @@ const Stack = createStackNavigator();
 
 const UploadRoutes:React.FC = () => {
 
+    const screenOptions = (title:string) => ({
+        title: title,
+        headerTintColor: '#fff',
+        headerStyle:{
+            backgroundColor: '#0b465e'
+        }
+    })
+
     return (
         <Stack.Navigator initialRouteName={'home'}>
                 <Stack.Screen
                     name={'home'}
                     component={Home}
-                    options={{ 
-                        headerShown: false
-                     }}
+                    options={{ headerShown: false }}
                 />
                 <Stack.Screen
                     name={'upload'}
-                    options={{ 
-                        title: 'Enviar comprovante',
-                        headerTintColor: '#0b465e'
-                     }}
+                    options={screenOptions('Enviar comprovante')}
                     component={UploadFile}
                 />
                 <Stack.Screen
                     name={'config'}
-                    options={{ 
-                        title: 'Configurações',
-                        headerTintColor: '#0b465e'
-                     }}
+                    options={screenOptions('Configurações')}
                     component={Config}
                 />
         </Stack.Navigator>
